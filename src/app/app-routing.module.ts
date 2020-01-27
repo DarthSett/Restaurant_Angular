56a31260443c9702ef767cc194b99e709ca74a39
DarthSett <sourav@hotcocoasoftware.com>
2020-01-27 13:15:00 +0530

    Done With Restaurant Management

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, CanActivate } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {RestlistComponent} from './restlist/restlist.component';
+import {RestaurantComponent} from './restaurant/restaurant.component' ;
+import {TestComponent} from './test/test.component';
+import {SuperAdminListComponent} from './super-admin-list/super-admin-list.component';
+import {SuperAdminComponent} from './super-admin/super-admin.component';
+import {OwnerListComponent} from './owner-list/owner-list.component';
+import {OwnerComponent} from './owner/owner.component';
+import {AdminListComponent} from './admin-list/admin-list.component';
+import {AdminComponent} from './admin/admin.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', component: DashboardComponent, canActivate: [AuthGuard], children: [
+      {path: 'rest', component: RestlistComponent, canActivate: [AuthGuard]},
+      {path: 'rest/:id', component: RestaurantComponent, canActivate: [AuthGuard]},
+      {path: 'superadmin', component: SuperAdminListComponent, canActivate: [AuthGuard]},
+      {path: 'superadmin/:id', component: SuperAdminComponent, canActivate: [AuthGuard]},
+      {path: 'admin', component: AdminListComponent, canActivate: [AuthGuard]},
+      {path: 'admin/:id', component: AdminComponent, canActivate: [AuthGuard]},
+      {path: 'owner', component: OwnerListComponent, canActivate: [AuthGuard]},
+      {path: 'owner/:id', component: OwnerComponent, canActivate: [AuthGuard]},
+      {path: '', redirectTo: 'rest', pathMatch: 'full'}
+   ]},
+  {path: 'test', component: TestComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,101 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import {
+  MatCardModule,
+  MatFormFieldModule,
+  MatListModule,
+  MatSelectModule,
+  MatInputModule,
+  MatIconModule,
+  MatGridListModule,
+  MatButtonModule,
+  MatExpansionModule,
+  MatTableModule,
+  MatDialogModule, MatTabsModule, MatTooltipModule, MatCheckboxModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BannerComponent } from './banner/banner.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RestlistComponent } from './restlist/restlist.component';
+import { LoginDetails } from './login-details';
+import { CustomerDetails } from './customer-details';
+import { Restaurant } from './restaurant';
+import {
+  DeleteRestaurantComponent,
+  RestaurantComponent,
+  UpdateFoodComponent,
+  UpdateRestaurantComponent
+} from './restaurant/restaurant.component';
+import {AgmCoreModule, MapsAPILoader} from '@agm/core';
+import { TestComponent } from './test/test.component';
+import {CreateUserComponent, DeleteUserComponent, SuperAdminListComponent} from './super-admin-list/super-admin-list.component';
+import { SuperAdminComponent } from './super-admin/super-admin.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminListComponent } from './admin-list/admin-list.component';
+import { OwnerListComponent } from './owner-list/owner-list.component';
+import { OwnerComponent } from './owner/owner.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BannerComponent,
+    SignupComponent,
+    HomeComponent,
+    HomeComponent,
+    DashboardComponent,
+    RestlistComponent,
+    RestaurantComponent,
+    UpdateRestaurantComponent,
+    DeleteRestaurantComponent,
+    UpdateFoodComponent,
+    TestComponent,
+    SuperAdminListComponent,
+    SuperAdminComponent,
+    CreateUserComponent,
+    DeleteUserComponent,
+    AdminComponent,
+    AdminListComponent,
+    OwnerListComponent,
+    OwnerComponent
   ],
   imports: [
+    FormsModule,
+    MatDialogModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatTableModule,
+    MatTabsModule,
+    MatTooltipModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDFiiAKvKjV8FMb-6hKlg6ZN5zx9ne6h_M'
+    }),
+    MatCheckboxModule
   ],
-  providers: [],
+  entryComponents: [UpdateRestaurantComponent, DeleteRestaurantComponent, UpdateFoodComponent, CreateUserComponent, DeleteUserComponent],
+  providers: [LoginDetails, CustomerDetails, Restaurant],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
